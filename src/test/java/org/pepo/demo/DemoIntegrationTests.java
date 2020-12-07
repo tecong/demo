447efebf56f8c985771e4b5f8df16d2ecdb97cc7
Ossi Hämäläinen <ossi.hamalainen@tieto.com>
2020-12-07 10:16:24 +0200

    Few unit tests

---
+++ src/test/java/org/pepo/demo/DemoIntegrationTests.java
@@ -0,0 +1,36 @@
+package org.pepo.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+
+import static org.assertj.core.internal.bytebuddy.matcher.ElementMatchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest(
+        webEnvironment = SpringBootTest.WebEnvironment.MOCK,
+        classes = DemoApplication.class)
+@AutoConfigureMockMvc
+public class DemoIntegrationTests {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private CarRepository repository;
+
+    @Test
+    public void getCars() throws Exception {
+        mvc.perform(get("/api/cars?model=A3").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content()
+                        .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$[0].model").value("A3"));
+
+    }
+}
