9875e431281b2381fa24ff617d593de02ed6d3b7
Ossi Hämäläinen <ossi.hamalainen@tieto.com>
2021-09-23 10:55:05 +0300

    Removed DB stuff

--- src/main/java/org/pepo/demo/DemoController.java
+++ src/main/java/org/pepo/demo/DemoController.java
@@ -17,9 +17,6 @@ import org.springframework.web.bind.annotation.*;
 public class DemoController {
 
     @Autowired
-    CarRepository carRepository;
-
-    @Autowired
     BuildProperties buildProperties;
 
     @GetMapping("/version")
@@ -29,79 +26,15 @@ public class DemoController {
     }
 
     @GetMapping("/calculate")
-    public ResponseEntity<Integer> getCalculation() {
+    public ResponseEntity<Double> getCalculation() {
         Random ran = new Random();
         int x = ran.nextInt(1000000) + 1000000;
         int i=0;
+        double y=0;
         for (i=0;i<x;i++) {
-            //
+            y+=Math.sqrt(i);
         }
-        ResponseEntity<Integer> _response = new ResponseEntity<>(Integer.valueOf(i), HttpStatus.OK);
+        ResponseEntity<Double> _response = new ResponseEntity<>(y, HttpStatus.OK);
         return _response;
-    }
-
-    @GetMapping("/cars")
-    public ResponseEntity<List<Car>> getAllCars(@RequestParam(required = false) String model) {
-        try {
-            List<Car> cars = new ArrayList<>();
-            if(model == null) {
-                carRepository.findAll().forEach(cars::add);
-            } else {
-                carRepository.findByModelContaining(model).forEach(cars::add);
-            }
-            if(cars.isEmpty()) {
-                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-            }
-
-            return new ResponseEntity<>(cars, HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
-
-    @GetMapping("/cars/{id}")
-    public ResponseEntity<Car> getCarById(@PathVariable("id") long id) {
-        Optional<Car> carData = carRepository.findById(id);
-
-        if(carData.isPresent()) {
-            return new ResponseEntity<>(carData.get(), HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-    }
-
-    @PostMapping("/cars")
-    public ResponseEntity<Car> createCar(@RequestBody Car car) {
-        try {
-            Car _car = carRepository.save(new Car(car.getModel(), car.getMake(), car.getYear()));
-            return new ResponseEntity<>(_car, HttpStatus.CREATED);
-        } catch (Exception e) {
-            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
-
-    @PutMapping("/cars/{id}")
-    public ResponseEntity<Car> updateCar(@PathVariable("id") long id, @RequestBody Car car) {
-        Optional<Car> carData = carRepository.findById(id);
-
-        if(carData.isPresent()) {
-            Car _car = carData.get();
-            _car.setModel(car.getModel());
-            _car.setMake(car.getMake());
-            _car.setYear(car.getYear());
-            return new ResponseEntity<>(carRepository.save(_car), HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
-
-    @DeleteMapping("/cars/{id}")
-    public ResponseEntity<HttpStatus> deleteCar(@PathVariable("id") long id) {
-        try {
-            carRepository.deleteById(id);
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
     }
 }
